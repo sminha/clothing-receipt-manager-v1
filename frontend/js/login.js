ad1e34c97a73eeb11ae59c123a7440be69dfd783
sminha <sminha8590@naver.com>
2024-11-21 01:57:33 +0900

    feat: Implement login form handling script

---
+++ frontend/js/login.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault() 
+
+  const data = {
+    id: form.elements[0].value, 
+    password: form.elements[1].value, 
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), 
+    })
+
+    const responseData = await response.json()
+
+    alert(`${responseData.message}`)
+
+    if (!response.ok) {
+      return
+    }
+
+    window.location.href = 'mypage.html' 
+  } catch (error) {
+    console.error('Failed to login:', error)
+    alert('로그인 중 문제가 발생하였습니다. 다시 시도해 주세요.')
+  }
+})
