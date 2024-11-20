eccf0140e489c860413f5313f5ad3589fef5f951
sminha <sminha8590@naver.com>
2024-11-21 01:56:11 +0900

    feat: Implement signup form handlig script

---
+++ frontend/js/signup.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault()
+
+  const data = {
+    name: form.elements[0].value,
+    id: form.elements[1].value, 
+    password: form.elements[2].value,
+    confirmPassword: form.elements[3].value, 
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify(data), 
+    })
+
+    const responseData = await response.json()
+
+    alert(`${responseData.message}`)
+
+    if (!response.ok) {
+      return
+    }
+
+    window.location.href = 'login.html'
+  } catch (error) {
+    console.error('Failed to sign up:', error)
+    alert('회원가입 중 문제가 발생하였습니다. 다시 시도해 주세요.')
+  }
+})
